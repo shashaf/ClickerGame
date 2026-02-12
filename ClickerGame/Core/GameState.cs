2ccfeabdae0f7e98a3a03ae5da833d00e5d878cb
Alexander Fomin <sanfom24@gmail.com>
2026-02-12 08:02:05 +0300

    Реализовал основной класс-синглтон с состоянием игры

---
+++ ClickerGame/Core/GameState.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClickerGame.Core
+{
+    public class GameState
+    {
+        private static GameState _instance; // поле для экземпляра
+
+        public static GameState Instance // метод доступа к экземпляру
+        {
+            get
+            {
+                if (_instance == null)
+                    _instance = new GameState();
+
+                return _instance;
+            }
+        }
+
+        // Игровые параметры
+        public int Score { get; private set; } = 0;
+        public int ClickPower { get; private set; } = 1;
+        public int AutoClickPower { get; private set; } = 0;
+
+        public int UpgradeCost { get; private set; } = 10;
+
+        private GameState() { } // закрытый конструктор
+
+        public void AddClick()
+        {
+            Score += ClickPower;
+        }
+
+        public void AutoClick() 
+        {
+            Score += AutoClickPower;
+        }
+
+        public bool BuyUpgrade()
+        {
+            if (Score >= UpgradeCost) { 
+                Score = UpgradeCost;
+                ClickPower++;
+                UpgradeCost += 10;
+                return true;
+            }
+
+            return false;
+        }
+
+        public void Reset()
+        {
+            Score = 0;
+            ClickPower = 1;
+            AutoClickPower = 0;
+            UpgradeCost = 10;
+        }
+    }
+}
